8629e80bb64d3df3ab601c1ee29c864ee39bc61d
Rakshit-Varshney <75945445+Rakshit-Varshney@users.noreply.github.com>
2022-03-13 12:31:32 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent("canvas");
+	instializeInSetup(mario);
+	video=createCapture(VIDEO);
+	video.size(800,400);
+	video.parent("game_console");
+	poseNet=ml5.poseNet(video,modalLoaded);
+	poseNet.on('pose',gotposes);
+}
+
+function draw() {
+	game();
+}
+function modalLoaded(){
+	console.log("model is loaded");
+}
+function gotposes(results){
+	if(results.length>0){
+		console.log(results);
+		noseX=results[0].pose.nose.x;
+		noseY=results[0].pose.nose.y;
+	}
+}
+
+
+
+
+
